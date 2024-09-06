162ac892056bb9dd368e479a46f730ce05fdbd14
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-09-07 00:02:20 +0530

    java-game

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -4,7 +4,7 @@ public class CommonConstants {
 
     public static final String DATA_PATH = "resources/data.txt";
 
-    public  static final String IMAGE_PATH = "resources/1.png";
+    public static final String IMAGE_PATH = "resources/1.png";
 
     public static final String FONT_PATH = "resources/Cartoonero.ttf";
 
@@ -14,10 +14,11 @@ public class CommonConstants {
 
     public static final Color BACKGROUND_COLOR = Color.decode("#101820");
 
-    public static final Dimension FRAME_SIZE = new Dimension(540,760);
+    public static final Dimension FRAME_SIZE = new Dimension(540, 760);
 
-    public static final Dimension BUTTON_PANEL_SIZE = new Dimension(FRAME_SIZE.width,(int)(FRAME_SIZE.height * 0.42));
+    public static final Dimension BUTTON_PANEL_SIZE = new Dimension(FRAME_SIZE.width, (int) (FRAME_SIZE.height * 0.42));
 
-    public static final Dimension RESULT_DIALOG_SIZE = new Dimension((int)(FRAME_SIZE.width/2),(int)(FRAME_SIZE.height/6));
-    
+    public static final Dimension RESULT_DIALOG_SIZE = new Dimension((int) (FRAME_SIZE.width / 2),
+            (int) (FRAME_SIZE.height / 6));
+
 }
